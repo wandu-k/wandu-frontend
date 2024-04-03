81825cec5931b1b7925ef0c8d0a770ac5283da50
HOJUN LEE <hppmm@naver.com>
2024-04-04 00:33:12 +0900

    달력 메인페이지에 띄워요

---
+++ src/components/calendar/CalendarUi.js
@@ -0,0 +1,10 @@
+import Calendar from "react-calendar";
+const CalendarUi = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+};
+
+export default CalendarUi;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,11 @@
+import CalendarUi from "../components/calendar/CalendarUi";
 import UserInfoUi from "../components/userInfo/UserInfoUi";
 const MainPage = () => {
   return (
     <div>
       <div>Main Page</div>
       <UserInfoUi></UserInfoUi>
+      <CalendarUi></CalendarUi>
     </div>
   );
 };
