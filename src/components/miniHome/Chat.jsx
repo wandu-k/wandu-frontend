ba66e341e08df9c6fe0fa42c9c7bba129a3d87bd
LEE HOJUN <hppmm@naver.com>
2024-06-16 15:56:15 +0900

    채팅

--- src/components/miniHome/Chat.jsx
+++ src/components/miniHome/Chat.jsx
@@ -1,94 +1,131 @@
-import React, {useEffect, useState, useRef, useContext} from 'react';
-import {useForm} from 'react-hook-form';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Client} from '@stomp/stompjs';
-import SockJS from 'sockjs-client';
-import {MiniHomeContext} from "../../contexts/MiniHomeContext";
+import React, { useEffect, useState, useRef, useContext } from "react";
+import { useForm } from "react-hook-form";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+import { MiniHomeContext } from "../../contexts/MiniHomeContext";
+import { LoginContext } from "../../contexts/LoginContext";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Chat = () => {
-    const {register, handleSubmit, reset, formState: {errors}} = useForm();
-    const [messages, setMessages] = useState([]);
-    const stompClientRef = useRef(null);
-    const {miniHome} = useContext(MiniHomeContext)
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { errors },
+  } = useForm();
+  const stompClientRef = useRef(null);
+  const { miniHome } = useContext(MiniHomeContext);
+  const { userInfo } = useContext(LoginContext);
+  const [messages, setMessages] = useState([]);
 
-    useEffect(() => {
-        if (miniHome) {
-            // SockJS와 STOMP 클라이언트 설정
-            const socket = new SockJS('http://localhost:7090/ws-stomp');
-            const stompClient = new Client({
-                webSocketFactory: () => socket,
-                connectHeaders: {
-                    Authorization: localStorage.getItem("accessToken")// JWT 토큰을 헤더에 포함
-                },
-                debug: (str) => {
-                    console.log(str);
-                },
-                onConnect: () => {
-                    console.log('Connected');
-                    // 서버로부터 메세지를 수신하는 구독 설정
-                    stompClient.subscribe(`/room/${miniHome.hpId}`, (message) => {
-                        const receivedMessage = JSON.parse(message.body);
-                        setMessages((prevMessages) => [...prevMessages, receivedMessage]);
-                    });
-                },
-                onStompError: (frame) => {
-                    console.error('Broker reported error: ' + frame.headers['message']);
-                    console.error('Additional details: ' + frame.body);
-                }
-            });
+  useEffect(() => {
+    console.log("채팅 연결 중..");
+    if (miniHome) {
+      // SockJS와 STOMP 클라이언트 설정
+      const socket = new SockJS("http://localhost:7090/ws-stomp");
+      const stompClient = new Client({
+        webSocketFactory: () => socket,
+        connectHeaders: {
+          Authorization: localStorage.getItem("accessToken"), // JWT 토큰을 헤더에 포함
+        },
+        debug: (str) => {
+          console.log(str);
+        },
+        onConnect: () => {
+          console.log("Connected");
+          // 서버로부터 메세지를 수신하는 구독 설정
+          stompClient.subscribe(`/room/${miniHome.hpId}`, (message) => {
+            const data = JSON.parse(message.body);
+            setMessages((prevMessages) => [...prevMessages, data]);
+            console.log(data);
+          });
+        },
+        onStompError: (frame) => {
+          console.error("Broker reported error: " + frame.headers["message"]);
+          console.error("Additional details: " + frame.body);
+        },
+      });
 
-            stompClient.activate();
-            stompClientRef.current = stompClient;
+      stompClient.activate();
+      stompClientRef.current = stompClient;
+      loadChatList();
 
-            return () => {
-                if (stompClientRef.current) {
-                    stompClientRef.current.deactivate();
-                }
-            };
+      return () => {
+        if (stompClientRef.current) {
+          stompClientRef.current.deactivate();
         }
+      };
+    }
+  }, [miniHome?.hpId]);
 
-    }, [miniHome]);
+  const loadChatList = () => {
+    console.log("채팅 기록 불러오는 중..");
+    axios
+      .get(`/api/user/minihome/${miniHome.hpId}/chat`, {
+        headers: { Authorization: localStorage.getItem("accessToken") },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setMessages(response.data);
+      })
+      .catch((error) => {});
+  };
 
-    const onSubmit = (data) => {
+  const onSubmit = (data) => {
+    console.log("userInfo:", userInfo);
+    data.userId = userInfo.userId;
+    console.log("전송할 데이터:", data);
 
-        console.log(data);
-        // 메세지를 서버로 전송
-        if (stompClientRef.current && stompClientRef.current.connected) {
-            console.log("전송 준비 중")
-            stompClientRef.current.publish({
-                destination: `/send/${miniHome.hpId}`,
-                body: JSON.stringify(data)
-            });
-            reset();
-        }
-    };
+    console.log(data);
+    // 메세지를 서버로 전송
+    if (stompClientRef.current && stompClientRef.current.connected) {
+      console.log("전송 준비 중");
+      stompClientRef.current.publish({
+        destination: `/send/${miniHome.hpId}`,
+        body: JSON.stringify(data),
+      });
+      reset();
+    }
+  };
 
-    return (
-        <>
-            <div className="flex flex-col h-full gap-4 border rounded-2xl p-4">
-                <label className="text-xl font-bold">실시간 채팅</label>
-                <div className="flex flex-col h-full justify-end overflow-auto">
-                    {messages.map((msg, index) => (
-                        <div key={index} className="p-2">
-                            {msg.message}
-                        </div>
-                    ))}
-                </div>
+  return (
+    <>
+      <div className="flex flex-col h-full max-h-96 gap-4 border rounded-2xl">
+        <label className="text-xl font-bold pt-4 pl-4 pr-4">실시간 채팅</label>
+        <div className="flex flex-col h-full justify-end overflow-y-scroll gap-4 p-4">
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={
+                "flex flex-col" +
+                (msg.userId == userInfo.userId ? " items-end" : "")
+              }
+            >
+              <Link to={`/${msg.userId}/minihome`} className={"font-bold"}>
+                {msg.nickname}
+              </Link>
+              <div>{msg.message}</div>
             </div>
-            <div className="h-14 flex rounded-full border w-full pr-10 overflow-hidden relative">
-                <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                        placeholder="채팅 보내기"
-                        className="h-full w-full rounded-full p-4"
-                        {...register("message", {required: true})}
-                    ></input>
-                    <button type="submit" className="absolute right-6 bottom-0 top-0">
-                        <FontAwesomeIcon icon="fa-solid fa-paper-plane"/>
-                    </button>
-                </form>
-            </div>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+      <div className="h-14 flex rounded-full border w-full pr-10 overflow-hidden relative">
+        <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="채팅 보내기"
+            className="h-full w-full rounded-full p-4"
+            {...register("message", { required: true })}
+          ></input>
+          <button type="submit" className="absolute right-6 bottom-0 top-0">
+            <FontAwesomeIcon icon="fa-solid fa-paper-plane" />
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Chat;
